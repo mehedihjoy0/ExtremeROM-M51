a0d6b5b6997030473c45079929020ddf0b42fe1e
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-09-21 18:57:35 +0600

    Update customize.sh

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -45,16 +45,3 @@ if [[ "$SOURCE_SUPPORT_CUTOUT_PROTECTION" != "$TARGET_SUPPORT_CUTOUT_PROTECTION"
         sed -i "$(sed -n "/config_enableDisplayCutoutProtection/=" "$FTP") c$R" "$FTP"
     fi
 fi
-
-# Crok's RAM Managment Fix
-[ -f "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc" ] && rm -f "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc"
-{
-    echo "on post-fs-data"
-    echo "    exec_background -- /system/bin/cmd device_config set_sync_disabled_for_tests persistent"
-    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_cached_processes 256"
-    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_phantom_processes 2147483647"
-    echo "    exec_background -- /system/bin/cmd settings put global settings_enable_monitor_phantom_procs false"
-    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_empty_time_millis 43200000"
-    echo "    exec_background -- /system/bin/cmd"
-} >> "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc"
-SET_METADATA "system" "system/etc/init/ram_mgmt_fix.rc" 0 0 644 "u:object_r:system_file:s0"
