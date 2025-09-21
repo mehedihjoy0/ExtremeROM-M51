60f629d0c833149b51785fc1663669d6d211c006
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-09-21 18:56:57 +0600

    Update customize.sh

--- unica/mods/rezetprop/customize.sh
+++ unica/mods/rezetprop/customize.sh
@@ -1,6 +1,7 @@
 LOG "- Applying prop spoofer"
 
 {
+    echo ""
     echo "on property:service.bootanim.exit=1"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.flash.locked 1"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.vbmeta.device_state locked"
@@ -9,6 +10,11 @@ LOG "- Applying prop spoofer"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.warranty_bit 0"
     echo "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n sys.oem_unlock_allowed 0"
     echo "    exec u:r:init:s0 root root -- /system/bin/settings put global ram_expand_size_list 0,1,2,4,6,8"
+    echo "    exec u:r:init:s0 root root -- /system/bin/device_config set_sync_disabled_for_tests persistent"
+    echo "    exec u:r:init:s0 root root -- /system/bin/device_config put activity_manager max_cached_processes 256"
+    echo "    exec u:r:init:s0 root root -- /system/bin/device_config put activity_manager max_phantom_processes 2147483647"
+    echo "    exec u:r:init:s0 root root -- /system/bin/settings put global settings_enable_monitor_phantom_procs false"
+    echo "    exec u:r:init:s0 root root -- /system/bin/device_config put activity_manager max_empty_time_millis 43200000"
     echo ""
 } >> "$WORK_DIR/system/system/etc/init/hw/init.rc"
 
@@ -18,4 +24,3 @@ LINES="$(sed -n "/^(allow init init_exec\b/=" "$WORK_DIR/system/system/etc/selin
 for l in $LINES; do
     sed -i "${l} s/)))/ execute_no_trans)))/" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
 done
-
