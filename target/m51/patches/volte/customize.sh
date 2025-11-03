85ce06dc78c9c7a071f8fa32c97095e28b90ec44
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-11-03 20:13:58 +0600

    Delete target/m51/patches/volte/customize.sh

--- target/m51/patches/volte/customize.sh
+++
@@ -1,4 +0,0 @@
-DELETE_FROM_WORK_DIR "vendor" "bin/hw/rild"
-DELETE_FROM_WORK_DIR "vendor" "etc/init/init.vendor.rilchip.rc"
-DELETE_FROM_WORK_DIR "vendor" "etc/init/emservice.rc"
-DELETE_FROM_WORK_DIR "vendor" "etc/init/init.vendor.rilcommon.rc"
