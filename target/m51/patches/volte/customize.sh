ecd7d18b93c6ffe8496f035815c79f56fa7f4f90
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-11-03 15:56:34 +0600

    Fix missing newline in customize.sh

--- target/m51/patches/volte/customize.sh
+++ target/m51/patches/volte/customize.sh
@@ -1,4 +1,4 @@
 DELETE_FROM_WORK_DIR "vendor" "bin/hw/rild"
 DELETE_FROM_WORK_DIR "vendor" "etc/init/init.vendor.rilchip.rc"
 DELETE_FROM_WORK_DIR "vendor" "etc/init/emservice.rc"
-DELETE_FROM_WORK_DIR "vendor" "etc/init/init.vendor.rilcommon.rc""
+DELETE_FROM_WORK_DIR "vendor" "etc/init/init.vendor.rilcommon.rc"
